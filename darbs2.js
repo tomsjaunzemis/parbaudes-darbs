404e75561b1ffa7c9789a5962fe32f9735f58947
tomsjaunzemis <78410813+tomsjaunzemis@users.noreply.github.com>
2021-09-29 12:46:16 +0300

    Add files via upload

---
+++ darbs2.js
@@ -0,0 +1,40 @@
+function lapa1(){
+    window.open("index3.html");
+}
+function lapa2(){
+    window.open("darbs4.html");
+}
+function lapa3(){
+    window.open("darbs5.html");
+}
+
+function zimet()
+  {
+      
+var canvas = document.getElementById('circle');
+if (canvas.getContext)
+{
+var ctx = canvas.getContext('2d'); 
+var X = canvas.width / 2;
+var Y = canvas.height / 2;
+var R = 45;
+ctx.beginPath();
+ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+ctx.lineWidth = 3;
+ctx.strokeStyle = '#FF0000';
+ctx.fillStyle = "yellow"
+ctx.stroke();
+}
+}
+
+function poga(){
+    ("index.html")
+}
+function poga2(){
+    ("index.html")
+}
+function poga3(){
+    ("index.html")
+}
+
+    
